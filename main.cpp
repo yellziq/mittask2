229935cf27dcc5d38d60dff14aadcac1f3f5a090
Ani <ansahakyan1500@gmail.com>
2024-05-14 22:01:02 +0400

    число Фибоначчи, посл-ть с 0,1

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include <iostream>
 using namespace std;
 int fib(int n) {
-    if (n <= 2) {
+    if (n == 2 || n == 3) {
         return 1;
     }
-    else {
-        return fib(n - 1) + fib(n - 2);
+    if (n == 1) {
+        return 0;
     }
+    return fib(n - 1) + fib(n - 2);
 }
 
 int main() {
