27037f872396b69307377e44fa53770dfc357b97
Ani <ansahakyan1500@gmail.com>
2024-05-14 22:00:04 +0400

    число Фибоначчи, посл-ть с 1,1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    cout << "Hello World!\n";
+int fib(int n) {
+    if (n <= 2) {
+        return 1;
+    }
+    else {
+        return fib(n - 1) + fib(n - 2);
+    }
+}
+
+int main() {
+    int n;
+    cout << "значение n  ";
+    cin >> n;
+    int result = fib(n);
+    cout << "fib(" << n << ") = " << result << endl;
+    return 0;
 }
